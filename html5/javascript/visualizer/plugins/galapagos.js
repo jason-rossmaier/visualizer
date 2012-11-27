f210c7dc60304f57bf9078ff4a158c6a394ced33
Jacob Fischer <jacob.t.fischer@gmail.com>
2012-11-27 00:08:56 -0600

    Deleted useless commented out code

--- html5/javascript/visualizer/plugins/galapagos.js
+++ html5/javascript/visualizer/plugins/galapagos.js
@@ -113,44 +113,6 @@ $(document).ready(function() {
 				renderer.drawTexture("plant", "units", plant.x, plant.y, 1, 1);
 			}
 		}
-
-		/*function run() {
-		drawQuad(0,0,1,1,0,0,255);
-
-		// draw all the tiles
-		for(x = 0; x < mapWidth; x++) {
-			for(y = 0; y < mapHeight; y++) {
-				//drawQuad(x,y,1,1,0,166,0);
-				drawTexture("grass",x,y,1,1);
-			}
-		}
-		
-		// for each plant in the current turn
-		for(i in gamelog.turns[turn].Plants) {
-			var plant = gamelog.turns[turn].Plants[i];
-			//drawQuad(plant.x, plant.y, 1, 1, 32, 255, 32);
-			drawTexture("plants", plant.x, plant.y, 1, 1, 15);
-			drawTexture("plants", plant.x, plant.y, 1, 1, plant.size);
-		}
-
-		// for each plant in the current turn
-		for(i in gamelog.turns[turn].Creatures) {
-			var creature = gamelog.turns[turn].Creatures[i];
-			drawQuad(creature.x, creature.y, 1, 1, creature.owner ? 255 : 32, 32, creature.owner ? 32 : 255);
-			//drawTexture("creature_legs_back", creature.x, creature.y, 1, 1, creature.speed-1);
-			//drawTexture("creature_tails", creature.x, creature.y, 1, 1, creature.energy-1);
-			drawTexture("creature_misc", creature.x, creature.y, 1, 1, 2);
-			drawTexture("creature_misc", creature.x, creature.y, 1, 1, 0);
-			drawTexture("creature_misc", creature.x, creature.y, 1, 1, 1);
-			//drawTexture("creature_legs_front", creature.x, creature.y, 1, 1, creature.speed-1);
-			//drawTexture("creature_armors", creature.x, creature.y, 1, 1, creature.defense-1);
-			//drawTexture("creature_spikes", creature.x, creature.y, 1, 1, creature.defense-1);
-			//drawTexture("creature_jaws", creature.x, creature.y, 1, 1, creature.carnivorism-1);
-			//drawTexture("creature_teeth", creature.x, creature.y, 1, 1, creature.carnivorism-1);
-			//drawTexture("creature_eyes", creature.x, creature.y, 1, 1, creature.herbivorism-1);
-		}
-	}*/
-
 	}
 });
 
